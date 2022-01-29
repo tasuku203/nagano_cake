5b72c4fe99b6525603914db88fdee7720d6d4d28
EC2 Default User <ec2-user@ip-172-31-80-227.ec2.internal>
2022-01-29 13:10:45 +0000

    [Add]ジャンル一覧・編集ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admin, controllers:{
+  devise_for :customers,skip: [:password,], controllers:{
+    registrations: "customers/registrations",
+    sessions: 'customers/sessions'
+  }
+  devise_for :admin,skip: [:registration, :password,], controllers:{
     sessions: 'admin/sessions'
   }
   root to: 'homes#top'
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
 end
