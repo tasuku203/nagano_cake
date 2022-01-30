070774b04236c97a467b249eee959132132adf28
EC2 Default User <ec2-user@ip-172-31-80-227.ec2.internal>
2022-01-30 09:48:03 +0000

    [Add]itemコントローラー・画像に関するgem

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,20 @@
+class Admin::ItemsController < ApplicationController
+  def new
+  end
+
+  def index
+
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:new, :index, :create, :show, :edit, :update]
   end
 end
