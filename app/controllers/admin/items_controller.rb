0434a586d8641e74c90d8d28121e400055714a89
EC2 Default User <ec2-user@ip-172-31-80-227.ec2.internal>
2022-02-01 14:00:42 +0000

    [Add]商品一覧ページ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,34 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def index
-
+    @items = Item.all
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.uptate(item_params)
+    redirect_to admin_item(@item.id)
   end
-end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name)
+  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :genere
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+
+  attachment :image
 end
