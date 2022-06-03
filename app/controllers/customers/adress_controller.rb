e30d3a0d1b55073015651fce8748cd3715099cd6
tasuku203 <hideyama0101@gmail.com>
2022-06-03 12:57:14 +0000

    [Add]ヘッダーとその他作成

---
+++ app/controllers/customers/adress_controller.rb
@@ -0,0 +1,2 @@
+class Customers::AdressController < ApplicationController
+end
---
+++ app/controllers/customers/items_controller.rb
@@ -0,0 +1,13 @@
+class Customers::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
+end
+
+
